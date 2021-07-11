8231f054146c1c31f2d44316ed545806a9ea5ccb
Abigail de Joode <abbydiode@gmail.com>
2021-07-11 15:37:12 +0200

    Refactor project code

--- Program.cs
+++ Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.IO;
 
-namespace LtxMerger
-{
-    class Program
-    {
+namespace LtxMerger {
+    class Program {
         /// <param name="source">The original textures.ltx file</param>
         /// <param name="mods">The file from which the mods should be used</param>
         /// <param name="output">The file to output the merged collection to</param>
-        static void Main(FileInfo source, FileInfo mods, FileInfo output)
-        {
+        static void Main(FileInfo source, FileInfo mods, FileInfo output) {
             output = output ?? source;
             if (!source.Exists) { Console.WriteLine($"Could not find file {source.FullName}"); return; }
             if (!mods.Exists) { Console.WriteLine($"Could not find file {mods.FullName}"); return; }
--- TextureCollection.cs
+++ TextureCollection.cs
@@ -2,34 +2,27 @@
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace LtxMerger
-{
-    class TextureCollection
-    {
+namespace LtxMerger {
+    class TextureCollection {
         public SortedList<string, string> Associations { get; set; } = new SortedList<string, string>();
         public SortedList<string, string> Specifications { get; set; } = new SortedList<string, string>();
         public SortedList<string, int> Types { get; set; } = new SortedList<string, int>();
         private Regex _regex = new Regex(@"\s+(?<key>[a-z0-9\\\.,:_ \[\]]+)\s+=\s+(?<value>[a-z0-9\\\.,:_ \[\]]+)", RegexOptions.IgnoreCase);
 
-        public TextureCollection(string text)
-        {
+        public TextureCollection(string text) {
             this.Deserialize(text);
         }
 
-        public TextureCollection Deserialize(string text)
-        {
+        public TextureCollection Deserialize(string text) {
             GroupType lastGroup = GroupType.Unknown;
             string[] lines = text.Split('\n');
-            foreach (string line in lines)
-            {
+            foreach (string line in lines) {
                 GroupCollection groups = _regex.Match(line).Groups;
                 lastGroup = line.Contains("[association]") ? GroupType.Assocation : lastGroup;
                 lastGroup = line.Contains("[specification]") ? GroupType.Specification : lastGroup;
                 lastGroup = line.Contains("[types]") ? GroupType.Types : lastGroup;
-                if (groups.Count > 1)
-                {
-                    switch (lastGroup)
-                    {
+                if (groups.Count > 1) {
+                    switch (lastGroup) {
                         case GroupType.Assocation:
                             Associations[groups["key"].Value] = groups["value"].Value;
                             break;
@@ -49,47 +42,38 @@ namespace LtxMerger
             return this;
         }
 
-        public string Serialize()
-        {
+        public string Serialize() {
             StringBuilder output = new StringBuilder();
             output.AppendLine("[association]");
-            foreach (KeyValuePair<string, string> association in Associations)
-            {
+            foreach (KeyValuePair<string, string> association in Associations) {
                 output.AppendLine($"\t\t{association.Key} = {association.Value}");
             }
             output.AppendLine("[specification]");
-            foreach (KeyValuePair<string, string> specification in Specifications)
-            {
+            foreach (KeyValuePair<string, string> specification in Specifications) {
                 output.AppendLine($"\t\t{specification.Key} = {specification.Value}");
             }
             output.AppendLine("[types]");
-            foreach (KeyValuePair<string, int> type in Types)
-            {
+            foreach (KeyValuePair<string, int> type in Types) {
                 output.AppendLine($"\t\t{type.Key} = {type.Value}");
             }
             return output.ToString();
         }
 
-        public static TextureCollection operator +(TextureCollection x, TextureCollection y)
-        {
+        public static TextureCollection operator +(TextureCollection x, TextureCollection y) {
             TextureCollection textureCollection = x;
-            foreach (KeyValuePair<string, string> association in y.Associations)
-            {
+            foreach (KeyValuePair<string, string> association in y.Associations) {
                 textureCollection.Associations[association.Key] = association.Value;
             }
-            foreach (KeyValuePair<string, string> specification in y.Specifications)
-            {
+            foreach (KeyValuePair<string, string> specification in y.Specifications) {
                 textureCollection.Specifications[specification.Key] = specification.Value;
             }
-            foreach (KeyValuePair<string, int> type in y.Types)
-            {
+            foreach (KeyValuePair<string, int> type in y.Types) {
                 textureCollection.Types[type.Key] = type.Value;
             }
             return textureCollection;
         }
 
-        private enum GroupType
-        {
+        private enum GroupType {
             Unknown,
             Assocation,
             Specification,
